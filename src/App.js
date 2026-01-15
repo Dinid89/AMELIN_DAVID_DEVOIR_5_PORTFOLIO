c41067dab21b1112a22c496215738158a8381810
David.A <david.amelin62@gmail.com>
2026-01-15 11:00:08 +0100

    nav barre + pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Home from "./pages/Home";
+import Services from './pages/Services';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Mentions from './pages/Mentions';
+
+
+
+
+class App extends React.Component {
+  render () {
+    return (
+      <>
+      <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="#">John Doe</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/Home">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/Services">Services</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/Portfolio">Portfolio</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/Contact">Contact</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/Mentions">Mentions Légales</Link>
+        </li>
+      </ul>
     </div>
-  );
+</div>
+</nav>
+
+<Routes>
+  <Route path="/Home" element={<Home/>}></Route>
+  <Route path="./Services" element={<Services/>}></Route>
+  <Route path="./Portfolio" element={<Portfolio/>}></Route>
+  <Route path="./Contact" element={<Contact/>}></Route>
+  <Route path="./Mentions" element={<Mentions/>}></Route>
+</Routes>
+</>
+    )
+  }
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+     <div>
+        <h1>Bonjour Je suis John Doe</h1>
+        <h2>Développeur Web Full-Stack</h2>
+     </div>   
+    )
+}
+
+export default Home;
