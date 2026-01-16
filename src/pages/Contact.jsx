c78f712a48e26f57fe2e122197a5c4d54f5afa30
David.A <david.amelin62@gmail.com>
2026-01-16 15:59:36 +0100

    Initialisation page Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,58 @@
+import "./contact.css";
+
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>me contacter</p>
-    </div>
+    <main>
+      <div>
+        <div className="titleContact">
+          <h1>Contact</h1>
+          <p>Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact</p>
+        </div>  
+            
+        <section className="mainContact d-flex flex-column flex-lg-row">
+              <div className="formulaire d-flex flex-column">
+                  <h3 className="titleForm p-2">Formulaire de contact</h3>
+                  <form action="/Contact" method="post">
+                  <fieldset className="fieldset d-flex flex-column flex-lg-column">
+                  <div className="mb-1">
+                    <input type="text" className="form-control" aria-label="Votre nom" required placeholder="Votre nom"/>
+                  </div>
+
+                  <div className="mb-1">
+                    <input type="text" className="form-control" aria-label="Votre prénom" required placeholder="Votre prénom"/>
+                  </div>
+
+                  <div className="mb-1">
+                    <input type="tel" className="form-control" required aria-label="Votre téléphone" placeholder="Votre téléphone"/>
+                  </div>
+
+                  <div className="mb-1">
+                    <input type="email" className="form-control" aria-label="Votre mal" required placeholder="Votre mail"/>
+                  </div>
+
+                  <div className="mb-1">
+                    <input type="text" className="form-control" aria-label="sujet" required placeholder="Sujet"/>
+                  </div>
+
+                  <div className="mb-1">
+                    <textarea className="message" required placeholder="Votre message"/>
+                  </div>
+                  <div className="btnSubmit d-flex justify-content-center">
+                    <button type="submit" aria-label="Envoyer le message">Envoyer</button>
+                  </div>
+                </fieldset>
+                </form>
+              </div>
+
+              <div className="coordonnées d-flex-row flex-lg-row ">
+                    <h3 className="titleForm p-2">Mes coordonnées</h3>
+                  
+
+                </div>
+            </section>
+      </div>
+    </main>
   );
 };
 
