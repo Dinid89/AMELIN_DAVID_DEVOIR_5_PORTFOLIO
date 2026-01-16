b3480f16fcb562c404b157c9d49a8364b8cf7c4f
David.A <david.amelin62@gmail.com>
2026-01-16 09:07:02 +0100

    section 1 page acceuil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,15 @@ import heroBg from "../images/hero-bg.jpg";
 const Home = () => {
     return (
     <>
+    <div className="container-fluid px-0">
     <section className="Hero"> 
-        <div className="title">
-            <h1 className="hello">Bonjour, je suis John Doe</h1>
-            <h2 className="job">Développeur Web Full-Stack</h2>
+        <div className="title text-center">
+            <h1 className="hello display-2 lg-3">Bonjour, je suis John Doe</h1>
+            <h2 className="hello display-2 lg-3">Développeur Web Full-Stack</h2>
         </div>
+        <button className="btnInfo display-10 lg-5">En savoir plus</button>
     </section>
+    </div>
     </>  
     )
 }
